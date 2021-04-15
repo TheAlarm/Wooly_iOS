7a7f2bb366e8a94c98f3f2ae4ceda68d8ea9fd5d
seu11ee <yslee2504@naver.com>
2021-04-15 21:20:26 +0900

    [add] UIView extension

--- Wooly/Wooly/Controllers/LoginByEmailVC.swift
+++ Wooly/Wooly/Controllers/LoginByEmailVC.swift
@@ -13,9 +13,15 @@ class LoginByEmailVC: UIViewController {
     
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var dividerView: UIView!
+    @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var findPasswordButton: UIButton!
+    @IBOutlet weak var dividerView: UIView!
+    @IBOutlet weak var signUpLabel: UILabel!
+    @IBOutlet weak var signUpButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +30,18 @@ class LoginByEmailVC: UIViewController {
     
     func setStyle(){
         titleLabel.font = UIFont.notoSans(size: 20, family: .Bold)
+        emailLabel.font = UIFont.notoSans(size: 16, family: .Regular)
+        passwordLabel.font = UIFont.notoSans(size: 16, family: .Regular)
+        emailTextField.font = UIFont.notoSans(size: 16, family: .Regular)
+        emailTextField.borderStyle = .none
+        emailTextField.backgroundColor = .lightBackground
+        emailTextField.setBor
+        passwordTextField.font = UIFont.notoSans(size: 16, family: .Regular)
+        dividerView.backgroundColor = .lineGray
+        loginButton.titleLabel?.font = UIFont.notoSans(size: 16, family: .Bold)
+        findPasswordButton.titleLabel?.font = UIFont.notoSans(size: 12, family: .Regular)
+        signUpLabel.font = UIFont.notoSans(size: 10, family: .Regular)
+        signUpButton.titleLabel?.font = UIFont.notoSans(size: 16, family: .Bold)
     }
 
 }
--- Wooly/Wooly/Sources/Extension/UIColor+Extension.swift
+++ Wooly/Wooly/Sources/Extension/UIColor+Extension.swift
@@ -9,41 +9,44 @@ import Foundation
 import UIKit
 
 extension UIColor {
-
-  @nonobjc class var mainGreen: UIColor {
-    return UIColor(red: 132.0 / 255.0, green: 212.0 / 255.0, blue: 67.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var mainBlue: UIColor {
-    return UIColor(red: 82.0 / 255.0, green: 143.0 / 255.0, blue: 236.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var textGrayblue: UIColor {
-    return UIColor(red: 61.0 / 255.0, green: 65.0 / 255.0, blue: 70.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var gray1: UIColor {
-    return UIColor(white: 51.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var lineGray: UIColor {
-    return UIColor(red: 235.0 / 255.0, green: 236.0 / 255.0, blue: 242.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var mainPur: UIColor {
-    return UIColor(red: 147.0 / 255.0, green: 164.0 / 255.0, blue: 1.0, alpha: 1.0)
-  }
-
-  @nonobjc class var mainColor: UIColor {
-    return UIColor(red: 114.0 / 255.0, green: 136.0 / 255.0, blue: 251.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var btnUnactive: UIColor {
-    return UIColor(red: 205.0 / 255.0, green: 208.0 / 255.0, blue: 222.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var alart: UIColor {
-    return UIColor(red: 1.0, green: 124.0 / 255.0, blue: 124.0 / 255.0, alpha: 1.0)
-  }
-
+    
+    @nonobjc class var mainGreen: UIColor {
+        return UIColor(red: 132.0 / 255.0, green: 212.0 / 255.0, blue: 67.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var mainBlue: UIColor {
+        return UIColor(red: 82.0 / 255.0, green: 143.0 / 255.0, blue: 236.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var textGrayblue: UIColor {
+        return UIColor(red: 61.0 / 255.0, green: 65.0 / 255.0, blue: 70.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var gray1: UIColor {
+        return UIColor(white: 51.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var lineGray: UIColor {
+        return UIColor(red: 235.0 / 255.0, green: 236.0 / 255.0, blue: 242.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var mainPur: UIColor {
+        return UIColor(red: 147.0 / 255.0, green: 164.0 / 255.0, blue: 1.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var mainColor: UIColor {
+        return UIColor(red: 114.0 / 255.0, green: 136.0 / 255.0, blue: 251.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var btnUnactive: UIColor {
+        return UIColor(red: 205.0 / 255.0, green: 208.0 / 255.0, blue: 222.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var alart: UIColor {
+        return UIColor(red: 1.0, green: 124.0 / 255.0, blue: 124.0 / 255.0, alpha: 1.0)
+    }
+    @nonobjc class var lightBackground: UIColor {
+        return UIColor(red: 247.0 / 255.0, green: 249.0 / 255.0, blue: 253.0 / 255.0, alpha: 1.0)
+    }
+    
 }
