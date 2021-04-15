e112fcd77e4467ad74adc57946ba9e7caa78258e
seu11ee <yslee2504@naver.com>
2021-04-15 19:54:34 +0900

    [add] UIColor, UIFont extension

--- Wooly/Wooly/Controllers/LoginByEmailVC.swift
+++ Wooly/Wooly/Controllers/LoginByEmailVC.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class LoginByEmailVC: UIViewController {
-
+    
+    //MARK: - IBOutlet
+    
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dividerView: UIView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
---
+++ Wooly/Wooly/Sources/Extension/UIFont+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  UIFont+Extension.swift
+//  Wooly
+//
+//  Created by 이예슬 on 2021/04/15.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+    func hansSansBold(size: CGFloat) -> UIFont {
+        return UIFont(name: "SpoqaHanSansNeo-Bold", size: size)!
+    }
+    
+    func hansSansRegular(size: CGFloat) -> UIFont {
+        return UIFont(name: "SpoqaHanSansNeo-Regular", size: size)!
+    }
+    
+    func hansSansLight(size: CGFloat) -> UIFont {
+        return UIFont(name: "SpoqaHanSansNeo-Light", size: size)!
+    }
+    
+}
